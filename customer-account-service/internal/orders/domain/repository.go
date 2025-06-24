85f316360394ab4a8166184077640e8e647ab4d9
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-24 03:22:50 +0300

    feat: added order model implementation and order data access interface

---
+++ customer-account-service/internal/orders/domain/repository.go
@@ -0,0 +1,17 @@
+package domain
+
+import (
+	"context"
+	"customer-account-service/customer-account-service/internal/orders/domain/models"
+	"customer-account-service/customer-account-service/internal/orders/dtos"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type OrderRepository interface {
+	FindAll(ctx context.Context, params dtos.OrderQueryParams, customerId uuid.UUID) ([]models.Order, error)
+	FindById(ctx context.Context, id uuid.UUID, customerId uuid.UUID) (*models.Order, error)
+	Update(ctx context.Context, updatedOrder *models.Order) error
+	Transaction(fn func(txRepo OrderRepository) error) error
+	WithTx(tx *gorm.DB) OrderRepository
+}
