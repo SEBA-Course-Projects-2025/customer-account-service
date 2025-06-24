4a49eef2dc9ea3ee45c34f5c688a63e93c58de02
Ivan Yakovenko <ivan2005y9@gmail.com>
2025-06-24 03:30:25 +0300

    feat: added entrypoint

---
+++ customer-account-service/cmd/api/server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	accountRepository "customer-account-service/customer-account-service/internal/account/infrastructure/repository"
+	accountHandlers "customer-account-service/customer-account-service/internal/account/interfaces/handlers"
+	orderRepository "customer-account-service/customer-account-service/internal/orders/infrastructure/repository"
+	orderHandlers "customer-account-service/customer-account-service/internal/orders/interfaces/handlers"
+	"customer-account-service/customer-account-service/internal/shared/db"
+	handlers "customer-account-service/customer-account-service/internal/shared/handler"
+	"customer-account-service/customer-account-service/internal/shared/router"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+// @title Vendor Service API
+// @version 1.0
+// @description API for managing vendor account.
+
+// @schemes https
+// @host localhost:8082
+// @BasePath /api
+func main() {
+
+	dbUsed, err := db.ConnectDb()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	accountRepo := accountRepository.New(dbUsed)
+	orderRepo := orderRepository.New(dbUsed)
+
+	sharedHandler := &handlers.Handler{
+		AccountRepo: accountRepo,
+		OrderRepo:   orderRepo,
+		Db:          dbUsed,
+	}
+
+	accountHandler := &accountHandlers.AccountHandler{
+		Handler: sharedHandler,
+	}
+
+	orderHandler := &orderHandlers.OrderHandler{
+		Handler: sharedHandler,
+	}
+
+	mainRouter := router.SetUpRouter(accountHandler, orderHandler)
+
+	port := os.Getenv("API_PORT")
+
+	if port == "" {
+		port = "8082"
+	}
+
+	fmt.Println(time.Now())
+
+	if err := mainRouter.Run(":" + port); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+
+}
